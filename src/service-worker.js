bf3cf0ff7247818159e33c7f687db13c336b3dd1
mmetta <mfmetta@hotmail.com>
2021-01-11 19:08:48 -0300

    Atualização service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,7 +4,6 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {
-  console.log('skipWaiting' + msg.data.action)
   if (msg.data.action === 'skipWaiting') {
     self.skipWaiting()
   }
